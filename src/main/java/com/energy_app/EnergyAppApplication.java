bcf45355b3482e7c70b0bd48deb94d5625b0a371
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-12-15 20:04:40 +0100

    feat: Initial Spring Boot project

---
+++ src/main/java/com/energy_app/EnergyAppApplication.java
@@ -0,0 +1,13 @@
+package com.energy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyAppApplication.class, args);
+	}
+
+}
