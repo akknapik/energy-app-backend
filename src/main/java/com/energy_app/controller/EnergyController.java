33b227c20ba5e5aeaf92ec47dfe53e1bde49ec80
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-12-16 20:12:35 +0100

    feat: add methods for find optimal charging window

--- src/main/java/com/energy_app/controller/EnergyController.java
+++ src/main/java/com/energy_app/controller/EnergyController.java
@@ -1,11 +1,11 @@
 package com.energy_app.controller;
 
 import com.energy_app.model.dto.DailyMixDto;
+import com.energy_app.model.dto.OptimalWindowDto;
+import com.energy_app.model.external.ChargingRequest;
 import com.energy_app.service.EnergyService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,10 @@ public class EnergyController {
     @GetMapping("/mix")
     ResponseEntity<List<DailyMixDto>> getGenerationMix() {
         return ResponseEntity.ok(energyService.getGenerationMixForThreeDays());
+    }
+
+    @PostMapping("/optimal-charging")
+    ResponseEntity<OptimalWindowDto> getOptimalChargingWindow(@RequestBody ChargingRequest chargingRequest) {
+        return ResponseEntity.ok(energyService.findOptimalChargingWindow(chargingRequest));
     }
 }
--- src/main/java/com/energy_app/service/EnergyService.java
+++ src/main/java/com/energy_app/service/EnergyService.java
@@ -2,8 +2,10 @@ package com.energy_app.service;
 
 import com.energy_app.model.dto.DailyMixDto;
 import com.energy_app.model.dto.FuelDto;
+import com.energy_app.model.dto.OptimalWindowDto;
 import com.energy_app.model.enumeration.FuelType;
 import com.energy_app.model.external.CarbonIntensityResponse;
+import com.energy_app.model.external.ChargingRequest;
 import com.energy_app.model.external.Fuel;
 import com.energy_app.model.external.GenerationData;
 import org.springframework.stereotype.Service;
@@ -46,6 +48,25 @@ public class EnergyService {
         CarbonIntensityResponse carbonIntensityResponse = fetchGenerationMix(from, to);
 
         return calculateAveragesAndPercentage(carbonIntensityResponse);
+    }
+
+    public OptimalWindowDto findOptimalChargingWindow(ChargingRequest chargingRequest) {
+        if(chargingRequest.numberOfHours() < 1 || chargingRequest.numberOfHours() > 6) {
+            throw new IllegalArgumentException("Wrong number of hours");
+        }
+
+        OffsetDateTime start = snapToNextHalfHour(OffsetDateTime.now());
+        OffsetDateTime end = start.plusHours(48);
+
+        CarbonIntensityResponse carbonIntensityResponse = fetchGenerationMix(start.toString(), end.toString());
+
+        List<GenerationData> intervals = carbonIntensityResponse.data();
+        int windowSize = chargingRequest.numberOfHours() * 2;
+        if(intervals.size() < windowSize) {
+            throw new IllegalArgumentException("Not enough data from api");
+        }
+
+        return calculateBestWindow(intervals, windowSize);
     }
 
     private CarbonIntensityResponse fetchGenerationMix(String from, String to) {
@@ -107,5 +128,48 @@ public class EnergyService {
 
     private static double round2(double v) {
         return Math.round(v * 100.0) / 100.0;
+    }
+
+    private OffsetDateTime snapToNextHalfHour(OffsetDateTime time) {
+        int minute = time.getMinute();
+        OffsetDateTime baseTime;
+
+        if(minute < 30) {
+            baseTime = time.withMinute(0).withSecond(0).withNano(0);
+        } else {
+            baseTime = time.withMinute(30).withSecond(0).withNano(0);
+        }
+
+        return baseTime.plusMinutes(30);
+    }
+
+    private OptimalWindowDto calculateBestWindow(List<GenerationData> intervals, int windowSize) {
+        double maxTotalPerc = -1.0;
+        int bestStartIndex = -1;
+
+        for(int i = 0; i <= intervals.size() - windowSize; i++) {
+            double currentSum = 0;
+            for(int j = 0; j < windowSize; j++) {
+                currentSum += calculateCleanEnergyPercentageInInterval(intervals.get(i+j));
+            }
+
+            if(currentSum > maxTotalPerc) {
+                maxTotalPerc = currentSum;
+                bestStartIndex = i;
+            }
+        }
+
+        GenerationData startInterval = intervals.get(bestStartIndex);
+        GenerationData endInterval = intervals.get(bestStartIndex + windowSize - 1);
+        double averagePerc = maxTotalPerc / windowSize;
+
+        return new OptimalWindowDto(startInterval.from(), endInterval.to(), round2(averagePerc));
+    }
+
+    private double calculateCleanEnergyPercentageInInterval(GenerationData generationData) {
+        return generationData.generationMix().stream()
+                .filter(fuel -> CLEAN.contains(fuel.fuelType()))
+                .mapToDouble(Fuel::percentage)
+                .sum();
     }
 }
